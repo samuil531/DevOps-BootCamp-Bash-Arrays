97e1f68935729b65cf4bc41fb2e7b7e62fee6817
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-09 18:21:48 +0300

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
-count=1
-for i in {a..z}; do
-    [[ $count -gt $1 ]] && break
-    mkdir folder_$i
-    ((count++))
-done
+
+folder_num=$1
+
+folder_count=1
+
+
+if [[ $folder_num -ge 1 ]] && [[ $folder_num -le 32 ]]
+then
+    for folder_index in {a..z}     
+    do
+        if [[ $folder_count -le $folder_num ]]
+        then
+            mkdir ./folder_$folder_index
+            let "folder_count=$folder_count+1"
+        else
+            exit 0
+        fi
+    done
+else
+    exit 1
+fi
+    for folder_index in {a..z}     
+    do
+        if [[ $folder_count -le $folder_num ]]
+        then
+            mkdir ./folder_a$folder_index
+            let "folder_count=$folder_count+1"
+        else
+            exit 0
+        fi
+    done
+else
+    exit 1
+fi
+
+#mkdir -p test/dir--{000..100}
+#touch test/dir--{000..100}/file-{A..Z}
+
+#mkdir "tmp_$file"
+#mv  "$file" "tmp_$file/$file"
