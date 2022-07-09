f66c794e08d92a10de67c311a3547a8fa7f18842
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-09 16:59:07 +0300

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-
-num=$1
-x=1
-
+count=1
 for i in {a..z}; do
-if [[ $x -le $num ]]
-then
-
-mkdir ./folder_$i
-((x++))
+    [[ $count -gt $1 ]] && break
+    mkdir folder_$i
+    ((count++))
 done
-
-if [[ $num -gt 1 ]]
-then
-echo "$num folders created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')";
-else
-echo "$num folder created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')"
-fi
-
